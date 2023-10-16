049a8ca890521f79736131901a07d67e16d1c3fa
Moroz Sergey <Zwerew2@mail.ru>
2023-10-16 23:11:09 +0300

    added OrderService and UserService

---
+++ TestTask/TestTask/Services/OrderService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask
+{
+	public class OrderService : IOrderService
+	{
+		public OrderService()
+		{
+		}
+
+        public Task<Order> GetOrder()
+        {
+            return null;
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return null;
+        }
+    }
+}
+
---
+++ TestTask/TestTask/Services/UserService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask
+{
+	public class UserService : IUserService
+	{
+        private readonly ApplicationDbContext dbContext;
+
+        public UserService(ApplicationDbContext dbContext)
+		{
+            this.dbContext = dbContext;
+        }
+
+        public Task<User> GetUser()
+        {
+            User usr = new User { Id = 1, Email = "user1@gmail.com", Status = UserStatus.Active };
+
+            return Task<User>.Factory.StartNew(() => usr);
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            User usr = new User { Id = 1, Email = "user1@gmail.com", Status = UserStatus.Active };
+            List<User> users = this.dbContext.Users.ToList();
+
+            
+            return Task<List<User>>.Factory.StartNew(() => users);
+        }
+    }
+}
+
